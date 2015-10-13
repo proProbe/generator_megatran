49b0672a6e62b25312f3c63227d18aa79065e619
Huy Tran <huytr402@student.liu.se>
2015-10-13 14:04:07 +0200

    [frontend] change gridlayout

--- public/init.js
+++ public/init.js
@@ -1,16 +1,31 @@
-$('.grid').masonry({
-	itemSelector: '.grid-item',
-	columnWidth: 200
-});
-$(function(){
+// $(document).ready(function() {
+// 	$('.grid').masonry({
+// 		columnWidth: 310,
+// 		itemSelector: '.grid-item'
+// 	});
+// });
+
+$('.grid').imagesLoaded(function() {
 	$('.grid').masonry({
-			// options
-			itemSelector : '.grid-item',
-			columnWidth: function( containerWidth ) {
-				return containerWidth / 5;
-			}
-		});
+		columnWidth: 310,
+		itemSelector: '.grid-item'
+	});
+	// $('.grid').masonry('reloadItems');
 });
+// $('.grid').masonry({
+// 	itemSelector: '.grid-item',
+// 	columnWidth: 200
+// });
+// $(function(){
+// 	$('.grid').masonry({
+// 			// options
+// 			itemSelector : '.grid-item',
+// 			// columnWidth: function( containerWidth ) {
+// 			// 	return containerWidth / 5;
+// 			// }
+// 			columnWidth: 100
+// 		});
+// });
 // var $container = $('.grid');
 // $container.imagesLoaded(function(){
 // 	$container.masonry({
